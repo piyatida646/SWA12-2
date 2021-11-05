fc22b118c6723795677094b302b43e7c30beff08
piyatida646kp <59428776+piyatida646@users.noreply.github.com>
2021-11-05 17:46:53 +0700

    Create DuckDriver.java

---
+++ src/DuckDriver.java
@@ -0,0 +1,24 @@
+
+
+public class DuckDriver {
+
+	public static void main(String[] args) {
+		Duck mduck = new MallardDuck();
+		mduck.performFly();
+		mduck.performQuack();
+		mduck.swim();
+		mduck.display();
+		mduck.setFlyBehavior(new FlyNoWay());
+		mduck.performFly();
+		mduck.setFlyBehavior(new FlyWithRocket());
+		mduck.performFly();
+		
+		Duck myDuck = new MyDuck();
+		myDuck.performFly();
+		myDuck.performQuack();
+		myDuck.swim();
+		myDuck.display();
+
+	}
+
+}
