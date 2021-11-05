bcf1d478231832de8063c8557ba67347a007d016
piyatida646kp <59428776+piyatida646@users.noreply.github.com>
2021-11-05 17:47:13 +0700

    Create MyDuck.java

---
+++ src/MyDuck.java
@@ -0,0 +1,16 @@
+
+
+public class MyDuck extends Duck {
+	
+	public MyDuck() {
+		quackBehavior = new MuteQuack();
+		flyBehavior = new FlyNoWay();
+	}
+	
+	@Override
+	public void display() {
+		System.out.println("My duck is cute");
+
+	}
+
+}
